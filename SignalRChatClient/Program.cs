c5ab9c308602fcae7fd82a1acda22b6ce40f633d
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2021-10-28 22:05:24 +0300

    Add project files.

---
+++ SignalRChatClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartChatAsync().ConfigureAwait(false).GetAwaiter();
+            Console.ReadKey();
+        }
+
+        static async Task StartChatAsync()
+        {
+            HubConnection connection
+               = new HubConnectionBuilder()
+               .WithUrl("https://localhost:5001/chat")
+               .Build();
+
+            connection.On<string, string>("ReceiveMessage",
+                (user, message) =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    Console.WriteLine(newMessage);
+                });
+
+            await connection.StartAsync();
+
+            string message;
+            do
+            {
+                message = Console.ReadLine();
+                await connection.InvokeAsync("SendMessage",
+                      "ITEA", message);
+            } while (!string.IsNullOrEmpty(message));
+        }
+    }
+}
